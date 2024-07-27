1e669b78fc59dd8f15c49a76da93e90443c6fcfa
Meet Patel <meet@meetpatel.xyz>
2024-07-27 15:08:02 -0400

    Implement EuropeanOption

---
+++ src/instruments/options/european_option.rs
@@ -0,0 +1,84 @@
+use std::fmt::Display;
+
+use crate::instruments::options::{Option, OptionType};
+use crate::instruments::{CallPutPayoff, EuropeanExercise, Exercise, Payoff, VanillaPayoff};
+use crate::money::{Currency, MonetaryNumber};
+
+pub struct EuropeanOption<N, C>
+where
+    N: MonetaryNumber,
+    C: Currency,
+{
+    payoff: VanillaPayoff<N, C>,
+    exercise: EuropeanExercise,
+}
+
+impl<N, C> EuropeanOption<N, C>
+where
+    N: MonetaryNumber,
+    C: Currency,
+{
+    pub fn new(payoff: VanillaPayoff<N, C>, exercise: EuropeanExercise) -> Self {
+        Self { payoff, exercise }
+    }
+}
+
+impl<N, C> Option<N, C> for EuropeanOption<N, C>
+where
+    N: MonetaryNumber,
+    C: Currency,
+{
+    fn get_option_type(&self) -> OptionType {
+        self.payoff.get_option_type()
+    }
+
+    fn get_payoff(&self) -> impl Payoff<N> {
+        self.payoff
+    }
+
+    fn get_exercise(&self) -> impl Exercise {
+        self.exercise
+    }
+}
+
+impl<N, C> Display for EuropeanOption<N, C>
+where
+    N: MonetaryNumber,
+    C: Currency,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let type_letter = match self.get_option_type() {
+            OptionType::CALL => "C",
+            OptionType::PUT => "P",
+        };
+        write!(
+            f,
+            "{} {} {}",
+            self.exercise.get_last_date().format("%y/%m/%d"),
+            self.payoff.get_strike(),
+            type_letter,
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::instruments::options::{EuropeanOption, OptionType};
+    use crate::instruments::{payoffs::VanillaPayoff, EuropeanExercise};
+    use crate::money::{currency::USD, Money};
+
+    use chrono::Utc;
+
+    #[test]
+    fn test_european_option_display() {
+        let strike_price: Money<f64, USD> = Money::new(30.00);
+        let option_type = OptionType::CALL;
+        let payoff = VanillaPayoff::new(strike_price, option_type);
+
+        let exercise = EuropeanExercise::new(Utc::now());
+
+        let option = EuropeanOption::new(payoff, exercise);
+
+        assert_eq!(option.to_string(), "24/07/27 $ 30.00 C");
+    }
+}
--- src/instruments/options/mod.rs
+++ src/instruments/options/mod.rs
@@ -1,2 +1,5 @@
 pub mod option;
 pub use option::*;
+
+pub mod european_option;
+pub use european_option::*;
